298e7baad4c5898bc7f4a3b9320d550a35181dfe
Mohit <mohitmalaviya1@gmail.com>
2024-08-14 22:22:48 +0530

    Initial commit

---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log(`DB Connection Success`))
+    .catch((err) => {
+      console.log(`DB Connection Failed`)
+      console.log(err)
+      process.exit(1)
+    })
+}
